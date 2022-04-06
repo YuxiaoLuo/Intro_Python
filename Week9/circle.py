3063995bb4aa47b356a55cbc602dcfaa4b2178bb
YuxiaoLuo <rollrainshell@gmail.com>
2022-04-06 11:16:41 -0400

    update week9 and add license

--- Week9/circle.py
+++ Week9/circle.py
@@ -7,10 +7,10 @@ import math
 # argument and returns the area of the circle
 
 def area(radius):
-    return math.pi * radius**2
+    return math.pi * radius**2 # pi*r^2
 
 # circumference function accepts a circle's 
 # radius and returns circle's circumference
 
 def circumference(radius):
-    return 2*math.pi * radius 
+    return 2*math.pi * radius # 2*pi*r
--- Week9/rectangle.py
+++ Week9/rectangle.py
@@ -2,7 +2,7 @@
 # calculations related to rectangles
 
 def area(width, length):
-    return width * length 
+    return width * length # area 
 
 def perimeter(width, length):
-    return (width + length) * 2
+    return (width + length) * 2 # perimeter
